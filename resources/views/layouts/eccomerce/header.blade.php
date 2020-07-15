8c72addbcfd34ae05592cb009a8078f484ea777e
sidik <sidik221b@gmail.com>
2020-07-16 03:43:26 +0700

    ganti asset jadi secure_asset

--- resources/views/layouts/eccomerce/header.blade.php
+++ resources/views/layouts/eccomerce/header.blade.php
@@ -7,28 +7,28 @@
 <!--===============================================================================================-->	
 	<link rel="icon" type="image/png" href="images/icons/favicon.png"/>
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="{{ asset('eccomerce') }}/vendor/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="{{ secure_asset('eccomerce') }}/vendor/bootstrap/css/bootstrap.min.css">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="{{ asset('eccomerce') }}/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="{{ secure_asset('eccomerce') }}/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="{{ asset('eccomerce') }}/fonts/iconic/css/material-design-iconic-font.min.css">
+	<link rel="stylesheet" type="text/css" href="{{ secure_asset('eccomerce') }}/fonts/iconic/css/material-design-iconic-font.min.css">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="{{ asset('eccomerce') }}/fonts/linearicons-v1.0.0/icon-font.min.css">
+	<link rel="stylesheet" type="text/css" href="{{ secure_asset('eccomerce') }}/fonts/linearicons-v1.0.0/icon-font.min.css">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="{{ asset('eccomerce') }}/vendor/animate/animate.css">
+	<link rel="stylesheet" type="text/css" href="{{ secure_asset('eccomerce') }}/vendor/animate/animate.css">
 <!--===============================================================================================-->	
-	<link rel="stylesheet" type="text/css" href="{{ asset('eccomerce') }}/vendor/css-hamburgers/hamburgers.min.css">
+	<link rel="stylesheet" type="text/css" href="{{ secure_asset('eccomerce') }}/vendor/css-hamburgers/hamburgers.min.css">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="{{ asset('eccomerce') }}/vendor/animsition/css/animsition.min.css">
+	<link rel="stylesheet" type="text/css" href="{{ secure_asset('eccomerce') }}/vendor/animsition/css/animsition.min.css">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="{{ asset('eccomerce') }}/vendor/slick/slick.css">
+	<link rel="stylesheet" type="text/css" href="{{ secure_asset('eccomerce') }}/vendor/slick/slick.css">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="{{ asset('eccomerce') }}/vendor/MagnificPopup/magnific-popup.css">
+	<link rel="stylesheet" type="text/css" href="{{ secure_asset('eccomerce') }}/vendor/MagnificPopup/magnific-popup.css">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="{{ asset('eccomerce') }}/vendor/perfect-scrollbar/perfect-scrollbar.css">
+	<link rel="stylesheet" type="text/css" href="{{ secure_asset('eccomerce') }}/vendor/perfect-scrollbar/perfect-scrollbar.css">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="{{ asset('eccomerce') }}/css/util.css">
-	<link rel="stylesheet" type="text/css" href="{{ asset('eccomerce') }}/css/main.css">
+	<link rel="stylesheet" type="text/css" href="{{ secure_asset('eccomerce') }}/css/util.css">
+	<link rel="stylesheet" type="text/css" href="{{ secure_asset('eccomerce') }}/css/main.css">
 <!--===============================================================================================-->
 </head>
 <body class="animsition">
